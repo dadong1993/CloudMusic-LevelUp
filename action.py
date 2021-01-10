6e047c1dbe5473809f936dda266be555bf5f4910
dadong1993 <71182539+dadong1993@users.noreply.github.com>
2021-01-10 14:17:40 +0800

    Update action.py

--- action.py
+++ action.py
@@ -69,9 +69,9 @@ class CloudMusic:
         loginUrl = "https://music.163.com/weapi/login/cellphone"
         self.loginData = self.enc.encrypt(
             json.dumps({
-                'phone': phone,
-                'countrycode': '86',
-                'password': password,
+                'phone': 15233585626,
+                'countrycode': '86',00000000000000000000000000000000000000000000000
+                'password': c2455e047728e2edfa5539dc49600082,
                 'rememberLogin': 'true'
             }))
         headers = {
